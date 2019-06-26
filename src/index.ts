7f0b731df539d18ecf8f2b3de82e5702a698ca28
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-06-26 23:56:19 +0200

    0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,49 @@
-import { get } from 'lodash'
+import { get } from 'lodash';
 
 interface IEqOperator {
-  _eq: string
+  _eq: string;
 }
 interface INeOperator {
-  _ne: string
+  _ne: string;
 }
 interface IInOperator {
-  _in: string[]
+  _in: string[];
 }
 interface INinOperator {
-  _nin: string[]
+  _nin: string[];
 }
 interface IGtOperator {
-  _gt: string
+  _gt: string;
 }
 interface ILtOperator {
-  _lt: string
+  _lt: string;
 }
 interface IGteOperator {
-  _gte: string
+  _gte: string;
 }
 interface ILteOperator {
-  _lte: string
+  _lte: string;
 }
 interface IIsNullOperator {
-  _is_null: boolean
+  _is_null: boolean;
 }
 interface ILikeOperator {
-  _like: string
+  _like: string;
 }
 interface INlikeOperator {
-  _nlike: string
+  _nlike: string;
 }
 interface IIlikeOperator {
-  _ilike: string
+  _ilike: string;
 }
 interface INilikeOperator {
-  _nilike: string
+  _nilike: string;
 }
 interface ISimilarOperator {
-  _similar: string
+  _similar: string;
 }
 interface INsimilarOperator {
-  _nsimilar: string
+  _nsimilar: string;
 }
 
 type IOperator =
@@ -63,48 +63,48 @@ type IOperator =
   | ISimilarOperator
   | INsimilarOperator
   | IValueTest
-  | string
+  | string;
 
 interface IAndTest {
-  _and: ITest[]
+  _and: ITest[];
 }
 interface IOrTest {
-  _or: ITest[]
+  _or: ITest[];
 }
 interface INotTest {
-  _not: ITest
+  _not: ITest;
 }
 interface IValueTest {
-  [key: string]: IOperator // TODO: allow only one single value
+  [key: string]: IOperator; // TODO: allow only one single value
 }
-type ITest = IAndTest | IOrTest | INotTest | IValueTest
+type ITest = IAndTest | IOrTest | INotTest | IValueTest;
 
 interface IFilterPermission {
-  filter: ITest
+  filter: ITest;
 }
 
 interface ISetPermission {
-  set: ITest
+  set: ITest;
 }
 interface IColumnPermission {
-  columns: string[]
+  columns: string[];
 }
 
 interface IInsertPermission extends ISetPermission, IColumnPermission {
-  check: ITest
+  check: ITest;
 }
 interface ISelectPermission extends IColumnPermission, IFilterPermission {
-  allow_aggregations?: boolean
+  allow_aggregations?: boolean;
 }
 interface IUpdatePermission extends ISetPermission, IColumnPermission, IFilterPermission {}
 // tslint:disable-next-line: no-empty-interface
 interface IDeletePermission extends IFilterPermission {}
 
 export interface IItemPermissions {
-  insert: IInsertPermission
-  select: ISelectPermission
-  update: IUpdatePermission
-  delete: IFilterPermission
+  insert: IInsertPermission;
+  select: ISelectPermission;
+  update: IUpdatePermission;
+  delete: IFilterPermission;
 }
 
 /**
@@ -115,101 +115,101 @@ export interface IItemPermissions {
  */
 export const isLike = (text: string, search: string, caseSentitive = true) => {
   if (typeof search !== 'string' || text === null) {
-    return false
+    return false;
   }
   // Remove special chars
-  search = search.replace(new RegExp('([\\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-])', 'g'), '\\$1')
+  search = search.replace(new RegExp('([\\.\\\\\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:\\-])', 'g'), '\\$1');
   // Replace % and _ with equivalent regex
-  search = search.replace(/%/g, '.*').replace(/_/g, '.')
+  search = search.replace(/%/g, '.*').replace(/_/g, '.');
   // Check matches
-  const flags = caseSentitive ? 'g' : 'gi'
-  return RegExp('^' + search + '$', flags).test(text)
-}
+  const flags = caseSentitive ? 'g' : 'gi';
+  return RegExp('^' + search + '$', flags).test(text);
+};
 
 export const isSimilar = (text: string, search: string) => {
   // TODO Code - the following line is an incorrect workaround
-  return isLike(text, search, false)
-}
+  return isLike(text, search, false);
+};
 const testValue = (
   value: any,
   currentTest: ITest | IValueTest,
   environment: any,
   initialPath: string = ''
 ): boolean => {
-  const cursor = currentTest as any
-  const field = Object.keys(currentTest)[0]
+  const cursor = currentTest as any;
+  const field = Object.keys(currentTest)[0];
   if (Object.keys(currentTest).length !== 1) {
     throw Error(
       `The permission should contain only one property, ${
         Object.keys(currentTest).length
       } found\n Test: ${JSON.stringify(currentTest)}`
-    )
+    );
   }
-  const currentPath = initialPath ? `${initialPath}.${field}` : field
-  const operand = Object.keys(cursor[field])[0]
-  const variableName = cursor[field][operand]
-  const fieldValue = get(value, currentPath)
+  const currentPath = initialPath ? `${initialPath}.${field}` : field;
+  const operand = Object.keys(cursor[field])[0];
+  const variableName = cursor[field][operand];
+  const fieldValue = get(value, currentPath);
   switch (operand) {
     case '_eq':
-      return environment[variableName] === fieldValue
+      return environment[variableName] === fieldValue;
     case '_ne':
-      return environment[variableName] !== fieldValue
+      return environment[variableName] !== fieldValue;
     case '_in':
-      return variableName.map((item: string) => environment[item]).includes(fieldValue)
+      return variableName.map((item: string) => environment[item]).includes(fieldValue);
     case '_nin':
-      return !variableName.map((item: string) => environment[item]).includes(fieldValue)
+      return !variableName.map((item: string) => environment[item]).includes(fieldValue);
     case '_gt':
-      return fieldValue > environment[variableName]
+      return fieldValue > environment[variableName];
     case '_lt':
-      return fieldValue < environment[variableName]
+      return fieldValue < environment[variableName];
     case '_gte':
-      return fieldValue >= environment[variableName]
+      return fieldValue >= environment[variableName];
     case '_lte':
-      return fieldValue <= environment[variableName]
+      return fieldValue <= environment[variableName];
     case '_is_null':
-      return !Boolean(fieldValue)
+      return !Boolean(fieldValue);
     case '_like':
-      return isLike(fieldValue, environment[variableName])
+      return isLike(fieldValue, environment[variableName]);
     case '_nlike':
-      return !isLike(fieldValue, environment[variableName])
+      return !isLike(fieldValue, environment[variableName]);
     case '_ilike':
-      return isLike(fieldValue, environment[variableName], false)
+      return isLike(fieldValue, environment[variableName], false);
     case '_nilike':
-      return !isLike(fieldValue, environment[variableName], false)
+      return !isLike(fieldValue, environment[variableName], false);
     case '_similar':
-      return isSimilar(fieldValue, environment[variableName])
+      return isSimilar(fieldValue, environment[variableName]);
     case '_nsimilar':
-      return !isSimilar(fieldValue, environment[variableName])
+      return !isSimilar(fieldValue, environment[variableName]);
     case undefined:
-      return false
+      return false;
     default:
-      return testValue(value, cursor[currentPath], environment, currentPath)
+      return testValue(value, cursor[currentPath], environment, currentPath);
   }
-}
+};
 
 const check = (value: any, checkTest: ITest, environment: any): boolean => {
-  const test = checkTest as any
-  const operand = Object.keys(test)[0]
+  const test = checkTest as any;
+  const operand = Object.keys(test)[0];
   switch (operand) {
     case '_and':
-      return test._and.every((subTest: ITest) => check(value, subTest, environment))
+      return test._and.every((subTest: ITest) => check(value, subTest, environment));
     case '_or':
-      return test._and.some((subTest: ITest) => check(value, subTest, environment))
+      return test._and.some((subTest: ITest) => check(value, subTest, environment));
     case '_not':
-      return !check(value, test._not, environment)
+      return !check(value, test._not, environment);
     default:
-      return testValue(value, test, environment)
+      return testValue(value, test, environment);
   }
-}
+};
 
 export const validateInsert = (value: any, permission: IInsertPermission, environment: any): boolean => {
-  return check(value, permission.check, environment)
-}
+  return check(value, permission.check, environment);
+};
 
 export const validateUpdate = (value: any, permission: IUpdatePermission, environment: any): boolean => {
-  return check(value, permission.filter, environment)
-}
+  return check(value, permission.filter, environment);
+};
 
 export const validateDelete = (value: any, permission: IDeletePermission, environment: any): boolean => {
-  return check(value, permission.filter, environment)
-}
+  return check(value, permission.filter, environment);
+};
